75745f419011f2a3a7f30b0fbf0a6089ceabe2f6
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-25 11:08:02 +0100

    added tests for HashSet class

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import HashMap from "./hashmap.js";
+import HashSet from "./hashset.js";
 
 const testHashMap = new HashMap();
 
@@ -54,3 +55,42 @@ console.log(testHashMap.entries());
 // Test for resizing
 testHashMap.set("moon", "silver");
 console.log(testHashMap.buckets);
+
+const testHashSet = new HashSet();
+
+// Test for set method
+testHashSet.set("apple");
+testHashSet.set("banana");
+testHashSet.set("carrot");
+testHashSet.set("dog");
+testHashSet.set("elephant");
+testHashSet.set("frog");
+testHashSet.set("grape");
+testHashSet.set("hat");
+testHashSet.set("ice cream");
+testHashSet.set("jacket");
+testHashSet.set("kite");
+testHashSet.set("lion");
+
+console.log(testHashSet.buckets);
+
+// Tests for has method
+console.log(testHashSet.has("apple"));
+console.log(testHashSet.has("dragon"));
+
+// Test for length method
+console.log(testHashSet.length());
+
+// Test for remove method
+testHashSet.remove("banana");
+console.log(testHashSet.buckets);
+
+// Test for length method
+console.log(testHashSet.length());
+
+// Test for keys method
+console.log(testHashSet.keys());
+
+// Test for resizing
+testHashSet.set("moon");
+console.log(testHashSet.buckets);
