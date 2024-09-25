d7de36787ba837b47d0170cfa8aa3106cd9c0341
Iulian Nichifor <73722884+Seven719@users.noreply.github.com>
2024-09-25 09:56:47 +0100

    added checkIndex method for error handling of the same error to make code more concise

--- hashmap.js
+++ hashmap.js
@@ -6,6 +6,12 @@ export default class HashMap {
     this.loadFactor = loadFactor;
   }
 
+  checkIndex(index) {
+    if (index < 0 || index >= buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+  }
+
   hash(key) {
     let hashCode = 0;
 
